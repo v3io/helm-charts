1b07c8280ab0e73b4f73cb95f8335ef965b77cfd
imakhlin <igorm@iguazio.com>
2019-03-19 09:04:17 +0200

    remove postgresql files from from mysql branch

--- stable/postgresql/files/docker-entrypoint-initdb.d/update-pb-hba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PG_HBA_FILE=$PGDATA/data/pg_hba.conf
-# Modify access policy to allow access to PostgreSQL from outside of the Docker container
-echo "" >> $PG_HBA_FILE
-echo "# Allow access from outside the Docker container" >> $PG_HBA_FILE
-echo "host    all             all             172.17.0.1/32           md5" >> $PG_HBA_FILE
-echo "host    all             all             0.0.0.0/32              md5" >> $PG_HBA_FILE
